d332cfcc5cd2c3aabf1378b65cd8ff8e3cd07e26
wisnu dani ardhana <wd46470@gmail.com>
2025-01-12 01:06:56 +0700

    Add files via upload

---
+++ Pertemuan 13/Segmentasi Thresholding Global.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+# Membaca gambar dalam grayscale
+image = cv2.imread('wajah.jpg', cv2.IMREAD_GRAYSCALE)
+
+# Menerapkan thresholding
+ret, thresh_image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+
+# Menampilkan hasil
+cv2.imshow('Thresholded image', thresh_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
